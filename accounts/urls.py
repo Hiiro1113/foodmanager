d1b33dc2eb6b01108fa3247b0dcebf5158d144d4
Danilo <bzbocexgtvo@gmail.com>
2021-06-11 18:48:34 +0900

    master

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', views.Login.as_view(), name='login'),
+    path('', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
 ]
